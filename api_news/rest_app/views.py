dc4d6529bae1d8b82dfc36a87b281da946253466
Igor <igorchernysh007@yandex.ru>
2023-11-29 17:02:16 -0300

    add order validation in views

--- api_news/rest_app/views.py
+++ api_news/rest_app/views.py
@@ -1,7 +1,6 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, serializers
 from parse_app.models import Post
 from .serializers import PostSerializer
-from rest_framework.response import Response
 from .mypagination import CustomPag
 # Create your views here.
 
@@ -27,4 +26,32 @@ class PostViewSet(viewsets.ModelViewSet):
         Method get_queryset writes the set to following order
         '''
         queryset = super().get_queryset()
-        return queryset.order_by(*self.request.query_params.getlist('order'))
+
+        # Get list of order=''
+        order_fields = self.request.query_params.getlist('order')
+        if order_fields:
+            # Check if 0st element has '-'
+            if order_fields[0].startswith('-'):
+                direction = '-'
+            else:
+                direction = ''
+
+            # Make field name without '-'
+            field_name = order_fields[0].lstrip('-')
+
+            # Check if order field does not exists in order_field list from Post model
+            invalid_field = False
+            for _ in order_fields:
+                if field_name not in self.ordering_fields:
+                    invalid_field = True
+                    break
+
+            # If Flag, return error
+            if invalid_field:
+                raise serializers.ValidationError(
+                    f" Invalid order field", code='500')
+
+            # Return query after validations
+            return queryset.order_by(f'{direction}{field_name}')
+
+        return queryset
